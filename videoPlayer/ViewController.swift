f45f117e81f54f5d1e107b493c9df61fc90686cf
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-16 22:00:44 +0300

    created a playVideo function and located the video

--- videoPlayer/ViewController.swift
+++ videoPlayer/ViewController.swift
@@ -20,3 +20,9 @@ class ViewController: UIViewController {
     }
 }
 
+private func playVideo() {
+    guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
+        debugPrint("test.mp4 not found")
+        return
+    }
+}
