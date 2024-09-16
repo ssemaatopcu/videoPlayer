9b3cd85e5379a260887bc1fb437f7fd969876b34
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-16 22:09:36 +0300

    called the function

--- videoPlayer/ViewController.swift
+++ videoPlayer/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        playVideo()
     }
     
     private func playVideo() {
