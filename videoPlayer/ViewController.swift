27e91a13c6f1531ee7fce6b4d1d9433127b7d80d
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-16 22:08:23 +0300

    played the video

--- videoPlayer/ViewController.swift
+++ videoPlayer/ViewController.swift
@@ -10,19 +10,25 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
-}
-
-private func playVideo() {
-    guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
-        debugPrint("test.mp4 not found")
-        return
+    
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
+            debugPrint("test.mp4 not found")
+            return
+        }
+        let  player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
     }
 }
