2de30d95463bc94ffae6764281d36c635431649f
Sema Topcu <ssemaatopcu.@gmail.com>
2024-09-16 21:43:22 +0300

    Initial Commit

---
+++ videoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  videoPlayer
+//
+//  Created by Sema Topcu on 9/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
